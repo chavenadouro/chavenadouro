9039890b3900f179defe95130f72cd643b514855
chavenadouro <cafechavenadouro@gmail.com>
2024-08-07 02:17:55 +0100

    Update update-special.php

--- update-special.php
+++ update-special.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     if (isset($data['dailySpecial'])) {
         $json_data = array('dailySpecial' => $data['dailySpecial']);
-        file_put_contents('daily-special.json', json_encode($json_data, JSON_PRETTY_PRINT));
+        file_put_contents('Shared_Data.json', json_encode($json_data, JSON_PRETTY_PRINT));
         echo 'Atualização bem-sucedida';
     } else {
         echo 'Dados inválidos';
