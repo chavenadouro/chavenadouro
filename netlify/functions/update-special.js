b8f4958b78628255dd33012e1d32fb5b34aad639
chavenadouro <cafechavenadouro@gmail.com>
2024-08-07 16:31:40 +0100

    Update update-special.js

--- netlify/functions/update-special.js
+++ netlify/functions/update-special.js
@@ -1,34 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-
-exports.handler = async function(event, context) {
+// netlify/functions/updateSpecial.js
+exports.handler = async (event, context) => {
     if (event.httpMethod !== 'POST') {
         return {
             statusCode: 405,
-            body: 'Method Not Allowed'
+            body: JSON.stringify({ message: 'Method Not Allowed' }),
         };
     }
 
-    try {
-        const data = JSON.parse(event.body);
-        if (data.dailySpecial) {
-            const filePath = path.join(__dirname, '..', '..', 'daily-special.json');
-            const jsonData = JSON.stringify({ dailySpecial: data.dailySpecial }, null, 2);
-            fs.writeFileSync(filePath, jsonData);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ success: true })
-            };
-        } else {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ success: false, message: 'Invalid data' })
-            };
-        }
-    } catch (error) {
+    const data = JSON.parse(event.body);
+
+    if (!data.dailySpecial) {
         return {
-            statusCode: 500,
-            body: JSON.stringify({ success: false, message: error.message })
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Invalid data' }),
         };
     }
+
+    // Lógica para processar a especialidade do dia
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Especialidade atualizada com sucesso!' }),
+    };
 };
